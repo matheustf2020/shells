3c2cb9f3c30f7221c61ad2b3be8e9987009c0e78
theudev <64526218+matheustf2020@users.noreply.github.com>
2021-07-16 03:28:13 -0300

    Add files via upload

---
+++ twopackages.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+read
+apt-get install gzip -y
+apt-get install nmap -y
+apt-get install net-tools -y
+apt-get install ssh* -y
+apt-get install build-essential -y
+apt-get install module-assistant -y
+apt-get install dkms -y
+apt-get install figlet -y
+apt-get install gzip -y
+apt-get install python-dev -y
+apt-get install synaptic -y
+apt-get install curl -y
+apt-get install perl -y
+apt-get install git -y
+apt-get install python-pip -y
+apt-get install python3-pip -y
+apt-get install snapd -y
+apt-get install ubuntu-restricted-extras -y
+apt-get install iw -y
+apt-get install 7zip* -y
+apt-get install aptitude -y
+apt-get install nmap -y
+apt-get install tshark -y
+apt-get install aptitude -y
+apt-get install crunch -y
+apt-get install wireless-tools -y
+apt-get install crunch -y
+apt-get install sftpd -y
+apt-get install ufw -y
+read
+exit
